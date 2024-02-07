3e7452321d6ba1a948ce1b817e94333a10b03dc9
spovondra <spovondra@gmail.com>
2024-02-07 01:03:11 +0100

    Add files via upload

--- app/src/main/java/com/example/denikv1/view/statistics/DailyStatisticsView.kt
+++ app/src/main/java/com/example/denikv1/view/statistics/DailyStatisticsView.kt
@@ -1,6 +1,7 @@
 package com.example.denikv1.view.statistics
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,12 +13,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.applandeo.materialcalendarview.CalendarView
-import com.applandeo.materialcalendarview.EventDay
 import com.example.denikv1.R
 import com.example.denikv1.controller.statistics.DailyStatisticsController
 import com.example.denikv1.controller.statistics.DailyStatisticsControllerImpl
 import com.example.denikv1.custom.CestaAdapter
+import com.example.denikv1.custom.customCalendar.date.DatePickerDialog
 import com.example.denikv1.model.CestaEntity
 import com.example.denikv1.model.CestaModel
 import com.example.denikv1.model.CestaModelImpl
@@ -38,21 +38,18 @@ interface DailyStatisticsView {
     fun updateGraph2(barEntries: List<BarEntry>, startDate: Long, endDate: Long)
 }
 
-class DailyStatisticsFragment : Fragment(), DailyStatisticsView {
+class DailyStatisticsFragment : Fragment(), DailyStatisticsView, DatePickerDialog.OnDateSetListener {
     private lateinit var controller: DailyStatisticsController
     private lateinit var cestaModel: CestaModel
-    private lateinit var calendarView: CalendarView
     private lateinit var barChart1: BarChart
     private lateinit var barChart2: BarChart
 
     private lateinit var layoutObtiznost: LinearLayout
-    private lateinit var layoutStylPrelezu: LinearLayout // Added the missing type
-
-    private val calendar: Calendar = Calendar.getInstance()
+    private lateinit var layoutStylPrelezu: LinearLayout
 
     private lateinit var recyclerViewRoutes: RecyclerView
 
-    private val events: MutableList<EventDay> = ArrayList()
+    private lateinit var dpd: DatePickerDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -63,92 +60,78 @@ class DailyStatisticsFragment : Fragment(), DailyStatisticsView {
         val statisticsModel = DailyStatisticsModelImpl(cestaModel)
         controller = DailyStatisticsControllerImpl(this, statisticsModel)
 
-        // Inicializace pohledů
+        // Initialize views
         barChart1 = view.findViewById(R.id.graph_obtiznost_denni)
         barChart2 = view.findViewById(R.id.graph_styl_prelezu)
         recyclerViewRoutes = view.findViewById(R.id.recyclerViewRoutesDailyStats)
-
-        calendarView = view.findViewById(R.id.calendarView)
-        // Sample events
-        val eventCalendar = Calendar.getInstance()
-        eventCalendar.add(Calendar.DAY_OF_MONTH, 1)
-
-        // Set events to CalendarView
-        calendarView.setEvents(events)
-
-        // Initialize views
         layoutObtiznost = view.findViewById(R.id.layout_obtiznost)
         layoutStylPrelezu = view.findViewById(R.id.layout_styl_prelezu)
 
-        calendarView.setOnDayClickListener(object : com.applandeo.materialcalendarview.listeners.OnDayClickListener {
-            override fun onDayClick(eventDay: EventDay) {
-                val selectedDate = eventDay.calendar.time
-                val startDate = Calendar.getInstance()
-                startDate.time = selectedDate
-                startDate.set(Calendar.HOUR_OF_DAY, 0)
-                startDate.set(Calendar.MINUTE, 0)
-                startDate.set(Calendar.SECOND, 0)
+        // Initialize the date picker dialog
+        initializeDatePicker()
 
-                val endDate = Calendar.getInstance()
-                endDate.time = selectedDate
-                endDate.set(Calendar.HOUR_OF_DAY, 23)
-                endDate.set(Calendar.MINUTE, 59)
-                endDate.set(Calendar.SECOND, 59)
-
-                (controller as DailyStatisticsControllerImpl).onDateChanged(
-                    startDate.get(Calendar.YEAR),
-                    startDate.get(Calendar.MONTH),
-                    startDate.get(Calendar.DAY_OF_MONTH)
-                )
-            }
-        })
-
-        // Set initial date for the controller
-        (controller as DailyStatisticsControllerImpl).onDateChanged(
-            calendar.get(Calendar.YEAR),
-            calendar.get(Calendar.MONTH),
-            calendar.get(Calendar.DAY_OF_MONTH)
-        )
-
-        // Nastavení aktivních a neaktivních dnů
-        lifecycleScope.launch {
-            val disabledDays = getDisabledDays()
-            calendarView.setDisabledDays(disabledDays)
-        }
+        showDatePicker()
 
         return view
     }
 
-    private suspend fun getDisabledDays(): List<Calendar> {
-        val startDate = Calendar.getInstance()
-        startDate.set(2024, Calendar.FEBRUARY, 1)
-        val endDate = Calendar.getInstance()
-        endDate.set(2024, Calendar.FEBRUARY, 29)
+    // Method to initialize the date picker dialog
+    private fun initializeDatePicker() {
+        val now: Calendar = Calendar.getInstance()
+        dpd = DatePickerDialog.newInstance(
+            this,
+            now.get(Calendar.YEAR),
+            now.get(Calendar.MONTH),
+            now.get(Calendar.DAY_OF_MONTH)
+        )
 
-        val disabledDays = mutableListOf<Calendar>()
+        dpd.setThemeDark(false)
+        dpd.accentColor = Color.parseColor("#9C27B0")
 
-        while (startDate.before(endDate)) {
-            if (!hasRouteOnDay(startDate)) {
-                // Přidat do seznamu neaktivních dnů
-                disabledDays.add(startDate.clone() as Calendar)
-            }
-            startDate.add(Calendar.DAY_OF_MONTH, 1)
+        val days = Array<Calendar>(13) { Calendar.getInstance() }
+        for (i in -6..6) {
+            val day: Calendar = Calendar.getInstance()
+            day.add(Calendar.DAY_OF_MONTH, i * 2)
+            days[i + 6] = day
         }
-        return disabledDays
+        dpd.setSelectableDays(days)
+
+        dpd.setOnCancelListener {
+            Log.d("DatePickerDialog", "Dialog was cancelled")
+        }
+
+        //dpd.onDateSetListener = this
     }
 
-    private suspend fun hasRouteOnDay(date: Calendar): Boolean {
-        val startDate = date.clone() as Calendar
+    // Method to show the date picker dialog
+    private fun showDatePicker() {
+        activity?.supportFragmentManager?.beginTransaction()
+            ?.replace(R.id.datePickerContainer, dpd)
+            ?.addToBackStack(null)
+            ?.commit()
+    }
+
+    override fun onDateSet(view: DatePickerDialog?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+        val selectedDate = Calendar.getInstance()
+        selectedDate.set(Calendar.YEAR, year)
+        selectedDate.set(Calendar.MONTH, monthOfYear)
+        selectedDate.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+        val startDate = selectedDate.clone() as Calendar
         startDate.set(Calendar.HOUR_OF_DAY, 0)
         startDate.set(Calendar.MINUTE, 0)
         startDate.set(Calendar.SECOND, 0)
 
-        val endDate = date.clone() as Calendar
+        val endDate = selectedDate.clone() as Calendar
         endDate.set(Calendar.HOUR_OF_DAY, 23)
         endDate.set(Calendar.MINUTE, 59)
         endDate.set(Calendar.SECOND, 59)
 
-        return cestaModel.hasRouteInDateRange(startDate.timeInMillis, endDate.timeInMillis)
+        (controller as DailyStatisticsControllerImpl).onDateChanged(
+            startDate.get(Calendar.YEAR),
+            startDate.get(Calendar.MONTH),
+            startDate.get(Calendar.DAY_OF_MONTH)
+        )
     }
 
     override fun updateGraph1(barEntries: List<BarEntry>, startDate: Long, endDate: Long) {
@@ -157,7 +140,10 @@ class DailyStatisticsFragment : Fragment(), DailyStatisticsView {
 
         if (barEntries.isNotEmpty()) {
             layoutObtiznost.visibility = View.VISIBLE
-            updateRecyclerViewCesty(startDate, endDate)
+            lifecycleScope.launch {
+                val cesty = cestaModel.getAllCestaForDateRange(startDate, endDate)
+                updateRecyclerViewCesty(cesty)
+            }
         } else {
             layoutObtiznost.visibility = View.GONE
             recyclerViewRoutes.adapter = null
@@ -170,25 +156,17 @@ class DailyStatisticsFragment : Fragment(), DailyStatisticsView {
 
         if (barEntries.isNotEmpty()) {
             layoutStylPrelezu.visibility = View.VISIBLE
-            updateRecyclerViewCesty(startDate, endDate)
+            lifecycleScope.launch {
+                val cesty = cestaModel.getAllCestaForDateRange(startDate, endDate)
+                updateRecyclerViewCesty(cesty)
+            }
         } else {
             layoutStylPrelezu.visibility = View.GONE
             recyclerViewRoutes.adapter = null
         }
     }
 
-    private fun updateRecyclerViewCesty(startDate: Long, endDate: Long) {
-        lifecycleScope.launch {
-            try {
-                val cesty = cestaModel.getAllCestaForDateRange(startDate, endDate)
-                setupRecyclerViewCesty(cesty)
-            } catch (e: Exception) {
-                Log.e("DailyStatisticsFragment", "Nelze načíst cesty", e)
-            }
-        }
-    }
-
-    private fun setupRecyclerViewCesty(cesty: List<CestaEntity>) {
+    private fun updateRecyclerViewCesty(cesty: List<CestaEntity>) {
         val layoutManager = LinearLayoutManager(requireContext())
         recyclerViewRoutes.layoutManager = layoutManager
         recyclerViewRoutes.adapter = CestaAdapter(cesty) { cestaId ->
